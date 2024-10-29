75d37eb599477c79365284d95a1c22a3753ba7f1
Lody <lodyiaremko@proton.me>
2024-10-29 19:13:00 +0100

    int and str variadic functions

--- main.c
+++ main.c
@@ -3,19 +3,26 @@
 #include <stdarg.h>
 
 void ft_putstr(char *str);
+void ft_putnbr(int nb);
 
 int ft_printf(const char *format, ...){
 int i = 0;
+
 va_list args;
 	
-	va_start( args, format);
+va_start(args, format);
 	
 	while (format[i] != '\0'){
 	if (format[i] == '%'){
 		i++;
-		if (format[i] == 's')
+		if (format[i] == 's'){
 			i++;
-			ft_putstr(va_arg( args,  char * ));
+			ft_putstr(va_arg(args,char *));
+		}
+		else if (format [i] == 'd'){
+			i++;
+			ft_putnbr(va_arg (args,int));
+		}
 	}
 	write(1, &format[i], 1);
 	i++;
@@ -24,7 +31,14 @@ va_end (args);
 return (i);
 }
 
+
+
+
+
+
+
+
 int main(void){
-ft_printf("Hello, i am %s \n", "variatic argument");
+ft_printf("Hello, i am %s, and this is my try number %d \n", "variatic argument", 1);
 return(0);
 }
