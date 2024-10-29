c42b389fe4a854271cbb15483351ed4c3c70eb81
Lody <lodyiaremko@proton.me>
2024-10-29 18:41:17 +0100

    variadic function in work

--- main.c
+++ main.c
@@ -2,25 +2,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void ft_putstr(char *str);
+
 int ft_printf(const char *format, ...){
 int i = 0;
 va_list args;
+	
+	va_start( args, format);
 	
 	while (format[i] != '\0'){
 	if (format[i] == '%'){
 		i++;
 		if (format[i] == 's')
 			i++;
-			write(1,"String found", 13);
+			ft_putstr(va_arg( args,  char * ));
 	}
 	write(1, &format[i], 1);
 	i++;
 	}
+va_end (args);
+return (i);
 }
 
 int main(void){
-char *str = "Hola %s \n";
-
-ft_printf(str);
+ft_printf("Hello, i am %s \n", "variatic argument");
 return(0);
 }
