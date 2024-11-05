906290814a95d984939341dd1a1ae53553584f4e
Volodymyr Iaremko <viaremko@c3r2s3.42malaga.com>
2024-11-05 18:12:46 +0100

    Cleaner code, bug fixed in the correct way

--- ft_printf.c
+++ ft_printf.c
@@ -7,30 +7,24 @@ va_list args;
 	
 va_start(args, format);
 	
-	while (format[i] != '\0'){
-	if (format[i] == '%'){
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 's')
+				ft_putstr(va_arg(args,char *));
+			else if (format [i] == 'd')
+				ft_putnbr(va_arg (args,int));
+			else if (format [i] == 'c')
+				ft_putchar(va_arg(args,int));
+			else if (format [i] == '%')
+				write(1,"%",1);
+		}
+		else
+			write(1, &format[i], 1);
 		i++;
-		if (format[i] == 's'){
-			i++;
-			ft_putstr(va_arg(args,char *));
-		}
-		else if (format [i] == 'd'){
-			i++;
-			ft_putnbr(va_arg (args,int));
-		}
-		else if (format [i] == 'c'){
-			i++;
-			ft_putchar(va_arg(args,int));
-		}
-		else if (format [i] == '%'){
-			i++;
-			write(1,"%",1);
-		}
-	}
-	if (format[i] != '%'){
-	write(1, &format[i], 1);
-	i++;
-	}
+		
 	}
 va_end (args);
 return (i);
